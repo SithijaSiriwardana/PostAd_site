827d4205b325eb1d971936f3550ba292260e4bfa
Sithija Siriwardana <sithijalawan@gmail.com>
2020-05-19 12:06:46 +0530

    ui change

---
+++ dist/precache-manifest.67f1bbc3124afa9a352351c063d34300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79da7820fe52a25360d235f08c64241e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe50de1e0e99d393c5a",
+    "url": "/static/css/main.92385f8b.chunk.css"
+  },
+  {
+    "revision": "5b850bcff263cc68e940",
+    "url": "/static/js/2.300d3374.chunk.js"
+  },
+  {
+    "revision": "b559361660d78b922d4d069960d98a25",
+    "url": "/static/js/2.300d3374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe50de1e0e99d393c5a",
+    "url": "/static/js/main.72a8e73f.chunk.js"
+  },
+  {
+    "revision": "4e2ecff1683e408ac732",
+    "url": "/static/js/runtime-main.7d5419c1.js"
+  }
+]);
